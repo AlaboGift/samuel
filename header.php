274f48ae08e00a0831f0aed1eed67b747a110534
Alabo Gift <alabo2015@yahoo.com>
2022-08-03 22:04:25 +0100

    removed preoloader

--- header.php
+++ header.php
@@ -24,7 +24,7 @@
 
    <body>
     <!-- Preloader Start -->
-    <div id="preloader-active">
+    <!-- <div id="preloader-active">
         <div class="preloader d-flex align-items-center justify-content-center">
             <div class="preloader-inner position-relative">
                 <div class="preloader-circle"></div>
@@ -33,7 +33,7 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> -->
     <!-- Preloader Start -->
     <header>
         <!-- Header Start -->
